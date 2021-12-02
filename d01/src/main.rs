ed35a2e31c4d91f4e69754df5ae04669c99d2a59
Sid Mitra <sidmitra.del@gmail.com>
2021-12-01 21:22:50 -0300

    Day 1 - part 2

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 
+// Part 2: 1749 is too high
+
 fn main() {
     let filename = "input.txt";
     let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
@@ -20,5 +22,29 @@ fn main() {
         last_num = num;
     }
 
-    println!("{:?}", increased_count);
+    // println!("Part 1: {:?}", increased_count);
+
+
+    // Part 2
+    let mut increased_count_two: i32 = 0;
+    let mut last_sum: i32 = 0;
+
+    // println!("window          | last_sum | current_sum");
+    for (idx, window) in numbers.windows(3).enumerate(){
+        // Skip comparison in the first iteration
+        if idx == 0{
+            last_sum = window.iter().sum();
+            continue
+        }
+
+        let current_sum: i32 = window.iter().sum();
+        // println!("{:?} | {:?}      | {:?}", window, last_sum, current_sum);
+
+        if current_sum > last_sum{
+            increased_count_two += 1;
+        }
+        last_sum = current_sum;
+    }
+
+    println!("Part 2: {:?}", increased_count_two);
 }
